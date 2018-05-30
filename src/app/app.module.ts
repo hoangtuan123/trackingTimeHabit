e89676220660b40f7064675ea1f7ae89f6f9b6f7
Tuan Hoang <nghoangtuan92@gmail.com>
2018-05-31 01:48:52 +0700

    CRUD with fireangular2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,20 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from '../environments/environment';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HabitListComponent } from './habit-list/habit-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HabitListComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/habit-list/habit-list.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireDatabase } from 'angularfire2/database';
+import * as moment from 'moment';
+import { AngularFireList } from 'angularfire2/database'
+
+@Component({
+  selector: 'app-habit-list',
+  templateUrl: './habit-list.component.html',
+  styleUrls: ['./habit-list.component.css']
+})
+export class HabitListComponent implements OnInit {
+  loggerList: AngularFireList<any>;
+
+  habitList: Habit[];
+  habits: Habit[];
+
+
+  dateNow: Date;
+  toDay: string;
+  habitNode: string;
+  loggerNode: string;
+
+  constructor(private db: AngularFireDatabase) {
+    this.dateNow = new Date();
+
+    this.habitNode = "habits";
+    this.loggerNode = "loggers";
+
+    this.toDay = moment(this.dateNow).format('YYYYMMDD');
+
+    this.loggerList = this.db.list(this.loggerNode + '/' + this.toDay);
+  }
+
+  onChangeModel(habit) {
+    habit.checked = !habit.checked;
+    this.db.list(this.loggerNode).set(this.toDay, this.habitList);
+  }
+
+  updateToDayHabit() {
+
+  }
+
+  ngOnInit() {
+    this.getDataByNode(this.habitNode).subscribe(
+      data => { this.habits = data },
+      err => { }
+    );
+    this.getDataByNode(this.loggerNode + '/' + this.toDay).subscribe(
+      data => this.proccessLogger(data),
+      err => this.proccessError(err),
+    )
+  }
+
+  getDataByNode(node): Observable<any[]> {
+    return this.db.list(node).valueChanges();
+  }
+
+  proccessError(err) {
+    console.log(err);
+  }
+
+  proccessLogger(data) {
+    console.log(data);
+    if (data && data.length) {
+      this.habitList = data;
+    }
+    else {
+      this.db.list(this.loggerNode).update(this.toDay, this.habits);
+    }
+
+  }
+}
+
+
+interface Habit {
+  key: string;
+  value: string;
+  checked: boolean;
+}
