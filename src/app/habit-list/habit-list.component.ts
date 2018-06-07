32aa9c7af8ca0982cbc90f246a7e285ad5213226
Tuan Hoang <nghoangtuan92@gmail.com>
2018-06-08 00:42:38 +0700

    update ui for app

--- src/app/habit-list/habit-list.component.ts
+++ src/app/habit-list/habit-list.component.ts
@@ -19,9 +19,12 @@ export class HabitListComponent implements OnInit {
 
 
   dateNow: Date;
-  toDay: string;
+  daySelected: string;
   habitNode: string;
   loggerNode: string;
+
+  days: any [] = [];
+  FORMAT_DATE = "YYYYMMDD";
 
   constructor(private db: AngularFireDatabase, public authService: AuthService) {
     this.dateNow = new Date();
@@ -29,31 +32,48 @@ export class HabitListComponent implements OnInit {
     this.habitNode = "habits";
     this.loggerNode = "loggers";
 
-    this.toDay = moment(this.dateNow).format('YYYYMMDD');
-
-    this.loggerList = this.db.list(this.loggerNode + '/' + this.toDay);
+    this.daySelected = moment(this.dateNow).format(this.FORMAT_DATE);
+    for(const item of [0,1,2,3,4,5,6,7]){
+      const date = moment(this.dateNow).subtract(item, 'days');
+      this.days.push({
+        date,
+        dateFormat: date.format(this.FORMAT_DATE),
+        dateString: date.format("LL"),
+        checked: (date.format(this.FORMAT_DATE) == this.daySelected ? true : false)
+      });
+    }
   }
 
-  onChangeModel(habit) {
-    habit.checked = !habit.checked;
-    this.db.list(this.loggerNode).set(this.toDay, this.habitList);
-  }
+  getLoggerList(day){
+    this.loggerList = this.db.list(this.loggerNode + '/' + day);
 
-  updateToDayHabit() {
-
-  }
-
-  ngOnInit() {
     this.habitObserveble = this.getDataByNode(this.habitNode);
 
     this.getDataByNode(this.habitNode).subscribe(
       data => { this.habits = data },
       err => { }
     );
-    this.getDataByNode(this.loggerNode + '/' + this.toDay).subscribe(
-      data => this.proccessLogger(data),
+    this.getDataByNode(this.loggerNode + '/' + day).subscribe(
+      data => this.proccessLogger(data, day),
       err => this.proccessError(err),
     )
+  }
+
+  onChangeModel(habit) {
+    console.log('log habits', habit);
+    habit.checked = !habit.checked;
+    this.db.list(this.loggerNode).set(this.daySelected, this.habitList);
+  }
+
+  onClickChoiceDay(day){
+    this.days.forEach(item => item.checked = false);
+    day.checked = true;
+    this.daySelected = day.dateFormat;
+    this.getLoggerList(day.dateFormat);
+  }
+
+  ngOnInit() {
+    this.getLoggerList(this.daySelected);
   }
 
   getDataByNode(node): Observable<any[]> {
@@ -64,15 +84,14 @@ export class HabitListComponent implements OnInit {
     console.log(err);
   }
 
-  proccessLogger(data) {
+  proccessLogger(data, day) {
     console.log(data);
     if (data && data.length) {
       this.habitList = data;
     }
     else {
-      this.db.list(this.loggerNode).update(this.toDay, this.habits);
+      this.db.list(this.loggerNode).update(day, this.habits);
     }
-
   }
 }
 
